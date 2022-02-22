1d9847e75ff3e172a65b4240ea06d87707cac87c
Dan Bradham <danielbradham@gmail.com>
2022-02-22 12:26:00 -0500

    Rename ContextSelectorDialog to ExtendedSubmitDialog

--- python/dialogs/__init__.py
+++ python/dialogs/__init__.py
@@ -11,4 +11,4 @@
 from .submit_dialog import SubmitDialog
 from .batch_render_dialog import BatchRenderDialog
 from .summary_dialog import SubmissionFailedDialog, SubmissionCompleteDialog
-from .context_selector_dialog import ContextSelectorDialog
+from .extended_submit_dialog import ExtendedSubmitDialog
